4d93d487e46ac6ef6837536908296a398b619775
norere <noree@kth.se>
2017-05-17 18:00:29 +0200

    Update main.go
    Now you can add an item :)

--- main.go
+++ main.go
@@ -1,64 +1,79 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type Item struct {
-  Name string
-  Value int
-}
+// "github.com/jinzhu/gorm"
+// _ "github.com/jinzhu/gorm/dialects/sqlite"
 
-type UserSession struct {
-  Items []Item
-}
+// type Item struct {
+// 	Name  string
+// 	Value int
+// }
 
-var db *gorm.DB
+// type UserSession struct {
+// 	Items []Item
+// }
 
-type Page struct {
-  gorm.Model
+// var db *gorm.DB
 
-	Title string `gorm:"column:title"`
-	Body  []byte `gorm:"column:body"`
-}
+// type Page struct {
+// 	gorm.Model
+
+// 	Title string `gorm:"column:title"`
+// 	Body  []byte `gorm:"column:body"`
+// }
 
 func main() {
-  tempDB, _ := gorm.Open("sqlite3", "book.db")
-  db = tempDB
-  defer db.Close()
-  
-  db.AutoMigrate(&Page{}) // if there's no table in the db, it will create one
+	// tempDB, _ := gorm.Open("sqlite3", "book.db")
+	// db = tempDB
+	// defer db.Close()
+
+	// db.AutoMigrate(&Page{}) // if there's no table in the db, it will create one
 
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/:title", func(c *gin.Context) {
-	  title := c.Param("title")
-	  
-	  var p Page
-	  
-	  if err := db.Where("title = ?", title).First(&p).Error; err != nil {
-	    c.AbortWithStatus(404)
-	    return
-	  }
-	  
-		c.HTML(200, "view.html", gin.H{
-		  "page": p,
+	// r.GET("/:title", func(c *gin.Context) {
+	// 	title := c.Param("title")
+
+	// 	var p Page
+
+	// 	if err := db.Where("title = ?", title).First(&p).Error; err != nil {
+	// 		c.AbortWithStatus(404)
+	// 		return
+	// 	}
+
+	// 	c.HTML(200, "view.html", gin.H{
+	// 		"page": p,
+	// 	})
+	// })
+
+	r.GET("/edit/:title/:position", func(c *gin.Context) {
+		// title := c.Param("title")
+		fmt.Println(c.Accepted)
+		c.HTML(200, "edit.tmpl", gin.H{})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.tmpl", gin.H{})
+	})
+
+	r.POST("/", func(c *gin.Context) {
+		name := c.PostForm("item_name")
+		price := c.PostForm("price")
+
+		c.HTML(200, "item.tmpl", gin.H{
+			"name":  name,
+			"price": price,
 		})
 	})
 
-	r.GET("/edit/:title", func(c *gin.Context) {
-	  title := c.Param("title")
-		c.HTML(200, "edit.html", gin.H{})
-	})
-
-	//r.StaticFS("/static", http.Dir("static"))
+	// //r.StaticFS("/static", http.Dir("static"))
 
 	r.Run(":8080") //r.Run(":" + os.Getenv("PORT"))
 }
